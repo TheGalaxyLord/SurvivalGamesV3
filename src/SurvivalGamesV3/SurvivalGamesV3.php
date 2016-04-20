380cde94c3d6d61190c7840dade0e0c2a18cb3be
Jake <jakehcasto@aol.com>
2016-04-20 09:56:33 -0400

    Update SurvivalGamesV3.php

--- src/SurvivalGamesV3/SurvivalGamesV3.php
+++ src/SurvivalGamesV3/SurvivalGamesV3.php
@@ -1,31 +1,38 @@
 <?php
-//Genisys branch!
+#Genisys branch!
 namespace SurvivalGamesV3;
+#Plugin
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\PluginTask;
 use pocketmine\event\Listener;
+#Events
 use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\command\CommandSender;
-use pocketmine\command\Command;
-use pocketmine\utils\TextFormat;
-use pocketmine\utils\Config;
-use pocketmine\math\Vector3;
-use pocketmine\level\Position;
-use pocketmine\Player;
-use pocketmine\block\Block;
-use pocketmine\tile\Sign;
-use pocketmine\level\Level;
-use pocketmine\item\Item;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\entity\EntityLevelChangeEvent; 
+use pocketmine\event\plugin\PluginEvent;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+use pocketmine\utils\TextFormat;
+use pocketmine\utils\Config;
+
+use pocketmine\math\Vector3;
+use pocketmine\level\Position;
+use pocketmine\level\Level;
 use pocketmine\entity\Effect;
-use pocketmine\event\entity\EntityLevelChangeEvent ; 
+
+use pocketmine\item\Item;
 use pocketmine\tile\Chest;
 use pocketmine\inventory\ChestInventory;
-use pocketmine\event\plugin\PluginEvent;
+use pocketmine\block\Block;
+use pocketmine\tile\Sign;
+
+use pocketmine\Player;
+
 class SurvivalGamesV3 extends PluginBase implements Listener {
     public $prefix = TextFormat::GRAY . "[" . TextFormat::WHITE . TextFormat::BOLD . "S" . TextFormat::RED . "G" . TextFormat::RESET . TextFormat::GRAY . "] ";
 	public $mode = 0;
